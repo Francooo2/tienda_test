6f135412cf4a0e57ce1c81936a0880cbc4d6e3cc
Franco Palma <fpalmalopez2@gmail.com>
2022-03-08 20:42:01 -0300

    Ajuste en controllers

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -1,9 +1,9 @@
 const pool = require('../db')
 
 const getProducts = async (req, res) => {
-    
+
     const sql = 'SELECT RTRIM(UPPER(name)) AS nameProduct, url_image, price, discount, (SELECT name FROM category WHERE id = product.category) AS nameCategory FROM product ORDER BY nameCategory DESC'
-    
+
     await pool.query(sql, (error, results) => {
         if (error) {
             res.status(404).send('Servicio no disponible.')
@@ -11,33 +11,21 @@ const getProducts = async (req, res) => {
             res.status(200).json(results)
         }
     })
-    
+
 }
 
 const getProduct = async (req, res) => {
-    
-    const name = req.params.name
-    
-    if( name[name.length-1] === '°' ) {
-        const sql = 'SELECT * FROM product WHERE SUBSTRING(RTRIM(UPPER(name)), 0, LENGTH(RTRIM(UPPER(name))) - 1) = ?'
-        await pool.query(sql, name.substring(0, name.length - 1), (error, results) => {
-            if (error) {
-                res.status(404).send('Servicio no disponible.')
-            } else {
-                res.status(200).json(results)
-            }
-        })
-    } else {
-        const sql = 'SELECT * FROM product WHERE RTRIM(UPPER(name)) = ?'
-        await pool.query(sql, name, (error, results) => {
-            if (error) {
-                res.status(404).send('Servicio no disponible.')
-            } else {
-                res.status(200).json(results)
-            }
-        })
-    }
 
+    const name = req.params.name
+    const sql = 'SELECT * FROM product WHERE RTRIM(UPPER(name)) = ?'
+
+    await pool.query(sql, name, (error, results) => {
+        if (error) {
+            res.status(404).send('Servicio no disponible.')
+        } else {
+            res.status(200).json(results)
+        }
+    })
 
 }
 
--- frontend/assets/js/filterFunctions.js
+++ frontend/assets/js/filterFunctions.js
@@ -7,9 +7,9 @@ export const filterNameProduct = (input, arrayNames) => {
     input.addEventListener('keyup', async (e) => {
     
         if ( arrayNames.includes(input.value.toUpperCase()) ) {
-            
+
             const product = await getMainData(`http://localhost:3000/${input.value.toUpperCase()}`)
-            
+
             for (let index = 0; index < cards.length; index++) {
                 if ( cards[index].children[2].textContent !== product[0].name.toUpperCase().trim() ) {
                     cards[index].classList.add('hide')
